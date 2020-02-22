dc5bd1c7edb7ce123c1750a03bf4d71f945faf42
Martin Cserép <martincserep1@gmail.com>
2020-02-22 13:07:27 +0100

    simple input

--- Bulls and Cows/main.cpp
+++ Bulls and Cows/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
+	constexpr int WORLD_LENGTH = 5;
+
 	cout << "Welcome to Bulls and Cows" << endl;
+	cout << "Can you guess the " << WORLD_LENGTH << " letter isogram I'm thinking of?" << endl;
+
+	string Guess = "";
+
+	cout << "Your guess: ";
+	cin >> Guess;
+	cout << Guess;
+
 	return 0;
 }
